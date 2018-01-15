37a8f243c32ed31bbabca339907719f7fd2241b0
lisx <15110256812@qq.com>
2018-01-15 17:29:02 +0800

    更新题库查询类型题

--- src/main/java/com/ducetech/hadmin/dao/IQuestionBankDao.java
+++ src/main/java/com/ducetech/hadmin/dao/IQuestionBankDao.java
@@ -22,6 +22,6 @@ public interface IQuestionBankDao extends IBaseDao<QuestionBank,Integer> {
     @Query(value="select o from QuestionBank o where (o.nodeCode like:station or o.nodeCode like:area or o.nodeCode='000') and o.ifUse=0")
     List<QuestionBank> findByStation(@Param("station") String station,@Param("area") String area);
 
-    @Query(value="SELECT COUNT(menuType) FROM Question WHERE questionBank=:bankId and menuType=:menuType")
+    @Query(value="SELECT COUNT(menuType) FROM Question WHERE questionBank=:bankId and menuType=:menuType and ifUse=0")
     Integer findByBank(@Param("bankId") QuestionBank bankId,@Param("menuType") String menuType);
 }
